d34214b1aa6db94c5591a1d2f839bf57076b587c
Darren Picard <darren.picard@neofinancial.com>
2021-09-07 21:35:06 -0600

    Set up start of new post route

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,72 @@
 use lambda_http::{
     handler,
-    http::StatusCode,
+    http::{Method, StatusCode},
     lambda_runtime::{self, Context, Error},
     Request, RequestExt, Response,
 };
-use log::LevelFilter;
+use log;
+use serde::{Deserialize, Serialize};
 use simple_logger::SimpleLogger;
+
+type MethodResponses = Result<Response<String>, Error>;
+
+#[derive(Serialize, Deserialize)]
+struct Brew {
+    name: String,
+    brew_type: String,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     SimpleLogger::new().init().unwrap();
-    lambda_runtime::run(handler(func)).await?;
+    lambda_runtime::run(handler(handler_func)).await?;
     Ok(())
 }
 
-async fn func(request: Request, _: Context) -> Result<Response<String>, Error> {
+async fn handler_func(request: Request, context: Context) -> MethodResponses {
+    let method = request.method();
+
+    match *method {
+        Method::GET => get_handler(request, context),
+        Method::POST => post_handler(request, context),
+        _ => unknown_method_handler(request, context),
+    }
+}
+
+fn get_handler(request: Request, _: Context) -> MethodResponses {
     let query_string = request.query_string_parameters();
+    let brew = query_string.get("brew_id");
 
-    log::info!("{:?}", query_string);
+    match brew {
+        Some(brew_id) => {
+            log::info!("Got successful get request for id {}", brew_id);
 
-    let first_name = match query_string.get("firstName") {
-        Some(name) => name,
-        None => {
-            let default_name = "Stranger";
-            log::warn!(
-                "User did not enter firstName defaulting to: {}",
-                default_name
-            );
-
-            default_name
+            Ok(Response::builder()
+                .status(StatusCode::OK)
+                .body(String::from(brew_id))
+                .unwrap())
         }
-    };
+        None => Ok(Response::builder()
+            .status(StatusCode::BAD_REQUEST)
+            .body(String::new())
+            .unwrap()),
+    }
+}
 
-    let response = Response::builder().status(StatusCode::OK);
+fn post_handler(_request: Request, _: Context) -> MethodResponses {
+    Ok(Response::builder()
+        .status(StatusCode::OK)
+        .body(String::from("response"))
+        .unwrap())
+}
 
-    Ok(response
-        .body(format!("Hello, my name is {}!", first_name))
+fn unknown_method_handler(request: Request, _: Context) -> MethodResponses {
+    log::warn!(
+        "Unknown method request made to lambda. Details: {:?}",
+        request
+    );
+    Ok(Response::builder()
+        .status(StatusCode::NOT_IMPLEMENTED)
+        .body(String::new())
         .unwrap())
 }
